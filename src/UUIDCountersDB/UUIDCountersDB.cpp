dfca4adcda430e8a7e31c6f218ed961712fb5865
Diego Fernández Barrera <bigomby@gmail.com>
2017-01-11 14:00:04 +0100

    :lipstick: Format using clang

--- src/UUIDCountersDB/UUIDCountersDB.cpp
+++ src/UUIDCountersDB/UUIDCountersDB.cpp
@@ -1,7 +1,7 @@
 /*
   Copyright (C) 2017 Eneo Tecnologia S.L.
   Authors: Diego Fernandez <bigomby@gmail.com>
-           Eugenio Perez <eupm90@gmail.com>
+	   Eugenio Perez <eupm90@gmail.com>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU Affero General Public License as
@@ -21,21 +21,21 @@
 
 using namespace EventsCounter;
 
-UUIDCountersDB::increment_result_t UUIDCountersDB::uuid_increment(
-		const std::string &t_uuid, const uint64_t n,
-		const counters_t *limits) {
+UUIDCountersDB::increment_result_t
+UUIDCountersDB::uuid_increment(const std::string &t_uuid,
+			       const uint64_t n,
+			       const counters_t *limits) {
 	counters_t::iterator it = this->counters.find(t_uuid);
 	if (it == this->counters.end()) {
 		return INCREMENT_NOT_EXISTS;
 	}
 
-	it->second+=n;
+	it->second += n;
 	if (!limits) {
 		return INCREMENT_OK;
 	}
 
-	const counters_t::const_iterator limits_it =
-		limits->find(t_uuid);
+	const counters_t::const_iterator limits_it = limits->find(t_uuid);
 	if (limits->end() == limits_it) {
 		return INCREMENT_NOT_EXISTS_IN_LIMITS;
 	} else if (it->second > limits_it->second) {
