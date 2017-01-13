404bcac6988225525ce7ef6ee49f5da6ed51f95d
Eugenio Pérez <eupm90@gmail.com>
2017-01-13 09:39:22 +0000

    Refactor UUIDConsumer
    * Add copyright notice
    * Add zero-Copy rapidjson parsing
    * Allow consume from many topics in UUIDKafkaConsumer
    * Constructor reusing (DRY) in UUIDKafkaConsumer
    * Reduce heap allocations in test tests/0001-UUIDConsumer.cpp

--- src/UUIDConsumer/UUIDConsumerKafka.cpp
+++ src/UUIDConsumer/UUIDConsumerKafka.cpp
@@ -1,3 +1,22 @@
+/*
+  Copyright (C) 2017 Eneo Tecnologia S.L.
+  Authors: Diego Fernandez <bigomby@gmail.com>
+     Eugenio Perez <eupm90@gmail.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <iostream>
 #include <memory>
 
@@ -8,21 +27,80 @@ using namespace rapidjson;
 using namespace std;
 using namespace EventsCounter;
 
+/// Wrapper class to give rapidjson zero-copy DOM parsing from a kafka message
+template <typename Encoding = UTF8<>> class SizedBufferStream {
+public:
+	typedef typename Encoding::Ch Ch; //!< Character type of the stream.
+
+	/// Default constructor
+	SizedBufferStream(Ch *t_msg, size_t t_msg_size)
+	    : payload(t_msg), size(t_msg_size), read_pos(0), write_pos(0) {
+	}
+
+	/// Read the current character from stream without moving the read
+	/// cursor.
+	Ch Peek() const {
+		return this->read_pos < this->size
+				       ? this->payload[this->read_pos]
+				       : '\0';
+	}
+
+	/// Read the current character from stream and moving the read cursor to
+	/// next character.
+	Ch Take() {
+		return this->read_pos < this->size
+				       ? this->payload[this->read_pos++]
+				       : '\0';
+	}
+
+	/// Get the current read cursor.
+	/// @return Number of characters read from start.
+
+	size_t Tell() {
+		return this->read_pos;
+	}
+
+	/// Write a character
+	void Put(Ch c) {
+		if (this->write_pos < this->size) {
+			this->payload[this->write_pos++] = c;
+		}
+	}
+
+	/// Begin writing operation at the current read pointer.
+	/// @return The begin writer pointer.
+	Ch *PutBegin() {
+		return &this->payload[this->write_pos = this->read_pos];
+	}
+	/// End the writing operation.
+	/// @param begin The begin write pointer returned by PutBegin().
+	/// @return Number of characters written.
+	size_t PutEnd(Ch *begin) {
+		return write_pos - static_cast<size_t>(begin - this->payload);
+	}
+
+private:
+	Ch *payload;
+	size_t size, read_pos, write_pos;
+};
+
 UUIDBytes UUIDConsumerKafka::consume(uint32_t timeout) const {
 	unique_ptr<RdKafka::Message> message(
 			this->kafka_consumer->consume(timeout));
 
-	switch (message->err()) {
-	case RdKafka::ERR__TIMED_OUT:
-		break;
-
-	case RdKafka::ERR__PARTITION_EOF:
-		break;
-
+	const int err = message->err();
+	switch (err) {
 	case RdKafka::ERR_NO_ERROR: {
-		const char *buf = (const char *)message->payload();
+		SizedBufferStream<> is(static_cast<char *>(const_cast<void *>(
+						       message->payload())),
+				       message->len());
 		Document json;
-		json.Parse(buf);
+		json.ParseStream<kParseDefaultFlags | kParseInsituFlag>(is);
+
+		if (json.HasParseError()) {
+			cerr << "Couldn't parse message JSON" << endl;
+			break;
+		}
 
 		if (!json.IsObject() || !json.HasMember("sensor_uuid") ||
 		    !json["sensor_uuid"].IsString()) {
@@ -35,6 +113,8 @@ UUIDBytes UUIDConsumerKafka::consume(uint32_t timeout) const {
 		return UUIDBytes(uuid_str, message->len());
 	}
 
+	case RdKafka::ERR__TIMED_OUT:
+	case RdKafka::ERR__PARTITION_EOF:
 	default:
 		break;
 	}
@@ -42,18 +122,12 @@ UUIDBytes UUIDConsumerKafka::consume(uint32_t timeout) const {
 	return UUIDBytes();
 }
 
-UUIDConsumerKafka::UUIDConsumerKafka(const char *brokers,
-				     const char *group_id,
-				     const char *topic) {
+/// Creates a new rkt conf with specified broker + group id
+static unique_ptr<RdKafka::Conf>
+new_rk_conf(const char *brokers, const char *group_id) {
 	std::string errstr;
-	std::vector<std::string> topics;
-
-	topics.push_back(topic);
-
 	unique_ptr<RdKafka::Conf> conf(
 			RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL));
-	unique_ptr<RdKafka::Conf> tconf(
-			RdKafka::Conf::create(RdKafka::Conf::CONF_TOPIC));
 
 	if (conf->set("group.id", group_id, errstr) != RdKafka::Conf::CONF_OK) {
 		throw "Error setting group id";
@@ -64,23 +138,19 @@ UUIDConsumerKafka::UUIDConsumerKafka(const char *brokers,
 		throw "Error setting broker list";
 	}
 
-	if (tconf->set("offset.store.method", "broker", errstr) !=
-	    RdKafka::Conf::CONF_OK) {
-		throw "Error setting group id";
-	}
+	return conf;
+}
 
-	if (tconf->set("auto.offset.reset", "smallest", errstr) !=
-	    RdKafka::Conf::CONF_OK) {
-		throw "Error setting offset";
-	}
+UUIDConsumerKafka::UUIDConsumerKafka(const char *brokers,
+				     const char *group_id,
+				     const vector<string> &topics)
+    : UUIDConsumerKafka(topics, new_rk_conf(brokers, group_id).get()) {
+}
 
-	if (conf->set("default_topic_conf", tconf.get(), errstr) !=
-	    RdKafka::Conf::CONF_OK) {
-		throw "Error setting topic conf";
-	}
-
-	this->kafka_consumer =
-			RdKafka::KafkaConsumer::create(conf.get(), errstr);
+UUIDConsumerKafka::UUIDConsumerKafka(const vector<string> &topics,
+				     RdKafka::Conf *conf) {
+	std::string errstr;
+	this->kafka_consumer = RdKafka::KafkaConsumer::create(conf, errstr);
 	if (!this->kafka_consumer) {
 		throw "Failed to create consumer";
 	}
--- src/UUIDConsumer/UUIDConsumerKafka.h
+++ src/UUIDConsumer/UUIDConsumerKafka.h
@@ -1,3 +1,22 @@
+/*
+  Copyright (C) 2017 Eneo Tecnologia S.L.
+  Authors: Diego Fernandez <bigomby@gmail.com>
+     Eugenio Perez <eupm90@gmail.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include "UUIDBytes.h"
@@ -17,20 +36,13 @@ public:
 	 */
 	UUIDConsumerKafka(const char *brokers,
 			  const char *group_id,
-			  const char *topic);
+			  const std::vector<std::string> &topic);
 
 	/**
 	 *
 	 */
-	UUIDConsumerKafka(RdKafka::Conf *t_kafka_consumer_conf,
-			  RdKafka::Conf *t_kafka_topic_conf);
-
-	/**
-	 *
-	 */
-	UUIDConsumerKafka(RdKafka::KafkaConsumer *t_kafka_consumer)
-	    : kafka_consumer(t_kafka_consumer) {
-	}
+	UUIDConsumerKafka(const std::vector<std::string> &topics,
+			  RdKafka::Conf *t_kafka_consumer_conf);
 
 	/**
 	 *
